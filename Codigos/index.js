7a2db0d96ad4967441b12933d2d0152211b35d18
kauahenrique-xx <kaua2hcantanhede@gmail.com>
2025-11-01 16:05:16 -0300

    Coloquei o fundo

--- Codigos/index.js
+++ Codigos/index.js
@@ -1,10 +1,12 @@
 // Variaveis
 let valorAtual = 0
 
-
-
 // Funções
 function clique() {
     valorAtual = valorAtual + 1;
     document.getElementById("valorNaTela").innerHTML = valorAtual   // Função de clique que adiciona o numero de
-}                                                                   //contagens quando vc clica
+                                                                    // clique
+}
+
+
+    
